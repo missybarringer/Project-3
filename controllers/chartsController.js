90baa2b8323cc96fa50b9af8dbc7e98f368c1c01
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-06-25 18:31:13 -0500

    added chart page & routing

---
+++ controllers/chartsController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the itemsController
+module.exports = {
+    findAll: function(req, res) {
+        db.Item
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.Item
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.Item
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.Item
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.Item
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
