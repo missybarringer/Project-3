011f1db7b2f0fa1ba8feb19f41c8c872f850ebc4
bwillstorey <bwillstorey@gmail.com>
2019-06-30 19:30:42 -0500

    reworked styling on stock page

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import { Col, Row, Card } from "react-bootstrap";
+import { Col, Row, Card, Container, Accordion, Button } from "react-bootstrap";
 import Media from 'react-bootstrap/Media';
 
 const ItemList = props => {
 
     return (
-        <div key={props.id} className="p-3 mb-3 bg-secondary shadow rounded">
-            <Media className="my-2">
-                <img
-                    width={100}
-                    src={props.image}
-                    alt={props.title}
-                />
-                <Media.Body
-                    className="ml-3"
-                >
-                    <Row>
-                        <Col sm={9}>
-                            <h5>{props.name}</h5>
-                        </Col>
-                        <Col sm={3} className="d-flex justify-content-end itemColor rounded">
-                            <h5>Quantity Remaining: {props.quantity}</h5>
-                        </Col>
-                    </Row>
-                    <Row className="pl-3">
-                        <p className="pr-3">Category: {props.category}</p>
-                        <p className="pr-3">Notes: {props.notes}</p>
-                    </Row>
-                </Media.Body>
-            </Media>
-        </div>
+        // <div key={props.id} className="shadow rounded" style={{fontSize: ".75rem"}}>
+        //     <Media className="my-2">
+        //         <Media.Body
+        //             className="ml-3"
+        //         >
+        //             <Row>
+        //                 <Col sm={9}>
+        //                     <h5>{props.name}</h5>
+        //                 </Col>
+        //                 <Col sm={3} className="d-flex justify-content-end itemColor rounded">
+        //                     <h5>Quantity Remaining: {props.quantity}</h5>
+        //                 </Col>
+        //             </Row>
+        //             <Row className="pl-3">Category: {props.category}</Row>
+        //             <Row className="pl-3">Notes: {props.notes}</Row>
+        //             <Row className="pl-3">Restock Date: {props.date}</Row>
+                    
+        //         </Media.Body>
+        //     </Media>
+        // </div>
 
+        <Accordion>
+            <Card className="stock-card">
+                <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        <Card.Title>{props.name}</Card.Title>
+                    </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Card.Text>Category: {props.category}</Card.Text>
+                        <Card.Text>Notes: {props.notes}</Card.Text>
+                        <Card.Text>Quantity Remaining: {props.quantity}</Card.Text>
+                        <Card.Text>Restock Date: {props.date}</Card.Text>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
     );
 };
 
