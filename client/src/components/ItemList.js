fb034b3bf9857dae1d3ff7a91d334272bb2ba119
bwillstorey <bwillstorey@gmail.com>
2019-07-02 19:08:18 -0500

    removing old stock page code from itemlist file

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import { Col, Row, Card, Container, Accordion, Button } from "react-bootstrap";
-import Media from 'react-bootstrap/Media';
+import { Card, Accordion, Button } from "react-bootstrap";
 
 const ItemList = props => {
 
     return (
-        // <div key={props.id} className="shadow rounded" style={{fontSize: ".75rem"}}>
-        //     <Media className="my-2">
-        //         <Media.Body
-        //             className="ml-3"
-        //         >
-        //             <Row>
-        //                 <Col sm={9}>
-        //                     <h5>{props.name}</h5>
-        //                 </Col>
-        //                 <Col sm={3} className="d-flex justify-content-end itemColor rounded">
-        //                     <h5>Quantity Remaining: {props.quantity}</h5>
-        //                 </Col>
-        //             </Row>
-        //             <Row className="pl-3">Category: {props.category}</Row>
-        //             <Row className="pl-3">Notes: {props.notes}</Row>
-        //             <Row className="pl-3">Restock Date: {props.date}</Row>
-                    
-        //         </Media.Body>
-        //     </Media>
-        // </div>
-
         <Accordion>
             <Card className="stock-card">
                 <Card.Header>
